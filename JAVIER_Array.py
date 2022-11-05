b3fa1989dc5ae1da147bafffbb591030abcf0617
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2022-11-06 01:40:18 +0800

    Added some flair to the main code.

--- JAVIER_Array.py
+++ JAVIER_Array.py
@@ -1,40 +1,78 @@
 from array import array
+import time 
+import sys
 
 def firstOption():
-    print("You have chosen to \033[0;34madd an element to the array. \033[0m")
+    print("\nYou have chosen to \033[0;34madd an element to the array. \033[0m")
     firstInput = int(input("\nEnter an element to add to the array: "))
     array.append(firstInput)
-    print(f"\nHere is your updated array: {array}")
-
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe element has been added succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
+    
 def secondOption():
-    print("You have chosen to \033[1;33minsert an element to the array. \033[0m")
+    print("\nYou have chosen to \033[1;33minsert an element to the array. \033[0m")
     secondInput = int(input("\nEnter an element to insert in the array: "))
     inputPosition = int(input("\nPlease indicate which position (0-9) you would like the element to be inserted: "))
     array.insert(inputPosition, secondInput)
-    print(f"\nHere is your updated array: {array}")
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe element has been inserted succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
 
 def thirdOption():
-    print("You have chosen to \033[0;32mmodify an element in the array. \033[0m")
+    print("\nYou have chosen to \033[0;32mmodify an element in the array. \033[0m")
     modifyPosition = int(input("\nPlease indicate the current position of the element (0-9) which you wish to modify: "))
     thirdInput = int(input("\nEnter a new element to replace the previous element with: "))
     array [modifyPosition] = thirdInput
-    print(f"\nHere is your updated array: {array}")
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe element has been modified succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
 
 def fourthOption():
-    print("You have chosen to \033[1;31mdelete an element in the array. \033[0m")
+    print("\nYou have chosen to \033[1;31mdelete an element in the array. \033[0m")
     fourthInput = int(input("\nEnter the element which you wish to remove from the array: "))
     array.remove(fourthInput)
-    print(f"\nHere is your updated array: {array}")
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe element has been removed succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
 
 def fifthOption():
     print("\nYou have chosen to \033[0;33marrange the array in ascending order. \033[0m")
     array.sort()
-    print(f"\nHere is your updated array: {array}")
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe array has been sorted succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
 
 def sixthOption():
     print("\nYou have chosen to \033[0;34marrange the array in descending order. \033[0m")
     array.sort(reverse=True)
-    print(f"\nHere is your updated array: {array}")
+    loading = ("\nUpdating the array...\n...\n")
+    for i in loading:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.1),
+    print("\n\033[0;32mThe array has been sorted succesfully.\033[0m")
+    print(f"\nHere is your new array: {array}")
     
 print("\nWelcome to \033[3mArray of Sun \033[0mwhere you can freely modify and arrange elements!\nSelect a number from the menu below and see the magic.")
 array = [1,5,4,3,2,10,7,9,8,6]
@@ -54,7 +92,7 @@ while True:
         break
 
     else: 
-        print("\nYou have chosen an invalid option. \033[3mPlease select an option from \033[4m1-6\033[0m in the menu.\033[0m\n")
+        print("\n\033[1;31mYou have chosen an invalid option.\033[0m\n\033[3mPlease select an option from \033[4m1-6\033[0m \033[3min the menu.\033[0m\n")
 
 if userInput == 1:
     firstOption()
